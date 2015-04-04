0be21659f1190e41db9b9818dd694015d94f78cc
Michael Egorov <michwill@gmail.com>
2015-04-04 15:20:55 -0700

    Set model instance attributes based on fields

--- zerodb/models/__init__.py
+++ zerodb/models/__init__.py
@@ -18,13 +18,24 @@ class Model(persistent.Persistent):
     """
 
     def __init__(self, **kw):
+        # This set will go to metaclass
         indexed_fields = set(filter(lambda key:
                 not key.startswith("_") and
                 isinstance(getattr(self.__class__, key), indexable.Indexable),
             self.__class__.__dict__.keys()))
 
+        # This set will go to metaclass
         required_fields = set(filter(lambda key: getattr(self.__class__, key).default is None, indexed_fields))
+
+        # Indexed fields which have default values
+        default_fields = indexed_fields.difference(required_fields).difference(kw)
 
         missed_fields = required_fields.difference(kw)
         if missed_fields:
             raise exceptions.ModelException("You should provide fields: " + ", ".join(map(str, missed_fields)))
+
+        for field in default_fields:
+            setattr(self, field, getattr(self.__class__, field).default)
+
+        for field, value in kw.iteritems():
+            setattr(self, field, value)
