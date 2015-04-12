4fdd686c2f7b4f7fc0c3f238c5b9861dcb24fb24
Michael Egorov <michwill@gmail.com>
2015-04-11 21:38:39 -0700

    Test that loadBulk ends up being one download request

--- test/test_batch.py
+++ test/test_batch.py
@@ -10,8 +10,11 @@ def zeo_storage(request, zeo_server):
 
 def test_loadBulk(zeo_storage):
     # oids in ZODB start from zero, and there are less than 10
+    count_0 = zeo_storage._debug_download_count
     oids = [("000000000000000%s" % i).decode('hex') for i in range(10)]
     out = zeo_storage.loadBulk(oids)
+    count_1 = zeo_storage._debug_download_count
+    assert count_1 - count_0 == 1
 
     assert len(out) == len(oids)
 
@@ -20,3 +23,7 @@ def test_loadBulk(zeo_storage):
 
     for oid in oids:
         assert zeo_storage._cache.load(oid)
+
+    for oit in oids:
+        zeo_storage.load(oid)
+    assert zeo_storage._debug_download_count == count_1
