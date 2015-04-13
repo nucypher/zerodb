1ef02ba4d6f969fa525121a7c1c4c733bcf031a6
Michael Egorov <michwill@gmail.com>
2015-04-12 17:56:47 -0700

    Some hints on performance testing

--- test/test_db.py
+++ test/test_db.py
@@ -17,12 +17,15 @@ def db(request, zeo_server):
 
 
 def test_query(db):
+    pre_request_count = db._storage._debug_download_count
     assert len(db[Page]) == 200
     assert len(db[Salary]) == 200
-    test_pages = db[Page].query(Contains("text", "something"), sort_index="title", limit=2)
+    test_pages = db[Page].query(Contains("text", "something"))
+    pre_range_count = db._storage._debug_download_count
+    assert pre_range_count - pre_request_count < 20  # We'll have performance testing separately this way
     test_salaries_1 = db[Salary].query(InRange("salary", 130000, 180000), sort_index="salary", limit=2)
     test_salaries_2 = db[Salary].query(InRange("salary", 130000, 130001), sort_index="salary", limit=2)
-    assert len(test_pages) == 2
+    assert len(test_pages) == 10
     assert len(test_salaries_1) == 2
     assert len(test_salaries_2) == 0
     for s in test_salaries_1:
