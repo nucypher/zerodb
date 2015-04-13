105dc38c3580bf43eca51a4eb9be58b5d7e9593c
Michael Egorov <michwill@gmail.com>
2015-04-12 18:03:39 -0700

    Test that we pre-downloaded objects into cache

--- test/test_db.py
+++ test/test_db.py
@@ -25,9 +25,12 @@ def test_query(db):
     assert pre_range_count - pre_request_count < 20  # We'll have performance testing separately this way
     test_salaries_1 = db[Salary].query(InRange("salary", 130000, 180000), sort_index="salary", limit=2)
     test_salaries_2 = db[Salary].query(InRange("salary", 130000, 130001), sort_index="salary", limit=2)
+    post_range_count = db._storage._debug_download_count
     assert len(test_pages) == 10
     assert len(test_salaries_1) == 2
     assert len(test_salaries_2) == 0
     for s in test_salaries_1:
         assert s.salary >= 130000
         assert s.salary <= 180000
+    # Check that we pre-downloaded all objects into cache
+    assert db._storage._debug_download_count == post_range_count
