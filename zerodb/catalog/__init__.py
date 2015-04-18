1d1ddb18a7ee487c2ed9366a833ff6cd101bdcd1
Michael Egorov <michwill@gmail.com>
2015-04-18 14:02:36 -0700

    Move catalog to catalog/__init__

--- zerodb/catalog/__init__.py
+++ zerodb/catalog/__init__.py
@@ -1,13 +1,13 @@
-import trees
+from copy import copy
+from repoze.catalog.indexes.field import CatalogFieldIndex as _CatalogFieldIndex
+from repoze.catalog.indexes.text import CatalogTextIndex as _CatalogTextIndex
+from repoze.catalog.catalog import Catalog as _Catalog
 from zope.index.text.lexicon import CaseNormalizer
 from zope.index.text.lexicon import Lexicon
 from zope.index.text.lexicon import Splitter
 from zope.index.text.lexicon import StopWordRemover
 from zope.index.text.okapiindex import OkapiIndex
-from copy import copy
-from repoze.catalog.indexes.field import CatalogFieldIndex as _CatalogFieldIndex
-from repoze.catalog.indexes.text import CatalogTextIndex as _CatalogTextIndex
-from repoze.catalog.catalog import Catalog as _Catalog
+from zerodb import trees
 
 
 class Catalog(_Catalog):
