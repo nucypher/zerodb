9a8d59fb1df01fa96da4e255f0735f230ec29f32
Michael Egorov <michwill@gmail.com>
2015-04-30 18:45:47 -0700

    More convenient virtualenv

--- virtualenv.sh
+++ virtualenv.sh
@@ -5,8 +5,12 @@ then
     virtualenv -p /usr/bin/python2.7 .virtual
     source .virtual/bin/activate
     python setup.py develop
+    if [ ! -e "activate" ]
+    then
+        ln -s .virtual/bin/activate .
+    fi
 else
     echo "All done already"
-    echo "  source .virtual/bin/activate -- activate virtual environment"
-    echo "  deactivate                   -- leave virtual environment"
+    echo "  source activate -- activate virtual environment"
+    echo "  deactivate      -- leave virtual environment"
 fi
