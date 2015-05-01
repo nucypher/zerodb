451c02044e6c17b2b5d02d50cc0b08662178eb02
Michael Egorov <michwill@gmail.com>
2015-05-01 01:22:12 -0700

    Crypto docstrings

--- zerodb/crypto/__init__.py
+++ zerodb/crypto/__init__.py
@@ -5,4 +5,11 @@ _rng = Random.new()
 
 
 def rand(size):
+    """
+    Generate a random string. In a cryptographically secure way
+
+    :param int size: How many bytes to generate
+    :return: Random string
+    :rtype: str
+    """
     return _rng.read(size)
--- zerodb/crypto/aes.py
+++ zerodb/crypto/aes.py
@@ -13,6 +13,11 @@ class AES(object):
     iv_size = 16  # Presumably depends on the mode though
 
     def __init__(self, passphrase=None, key=None, size=32):
+        """
+        :param str passphrase: Passphrase for encryption (use if no key is set)
+        :param str key: Symmetric key (if passphrase is not set)
+        :param int size: Size of a key to make from passphrase
+        """
         if passphrase is not None:
             assert key is None
             assert size <= 32
@@ -23,12 +28,22 @@ class AES(object):
         self._rand = Random.new()
 
     def encrypt(self, data):
+        """
+        :param str data: Data to encrypt
+        :return: Encrypted data with hash inside and IV outside
+        :rtype: str
+        """
         iv = self._rand.read(self.iv_size)
         cipher = CryptoAES.new(self.key, self.mode, iv)
         h = SHA256.new(data).digest()
         return cipher.encrypt(data + h) + iv  # with modes other than CFB we'd need to pad/unpad data
 
     def decrypt(self, edata):
+        """
+        :param str edata: Data to decrypt
+        :return: Decrypted data
+        :rtype: str
+        """
         data = edata[:-self.iv_size]
         iv = edata[-self.iv_size:]
         cipher = CryptoAES.new(self.key, self.mode, iv)
