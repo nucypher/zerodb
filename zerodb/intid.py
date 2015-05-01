beb85319a1876bbdbded2f980b66b5913d1b23d1
Michael Egorov <michwill@gmail.com>
2015-05-01 01:13:46 -0700

    intid docstrings

--- zerodb/intid.py
+++ zerodb/intid.py
@@ -8,23 +8,30 @@ from zerodb.trees import family32
 
 
 class IdStore(persistent.Persistent):
+    """
+    Persistent storage of objects which generates non-conflictiong unique IDs
+    for them
+    """
 
     _v_nextid = None
 
     family = family32
 
     def __init__(self, family=family32):
+        """
+        :param family: Family of BTrees to use
+        """
         self.tree = family.IO.BTree()
 
     def _generateId(self):
         """Generate an id which is not yet taken.
 
-        This tries to allocate sequential ids so they fall into the
-        same BTree bucket, and randomizes if it stumbles upon a
-        used one.
+        This tries to allocate sequential ids so they fall into the same BTree
+        bucket, and randomizes if it stumbles upon a used one.
 
-        This algorithm is taken from zope.intid but it will cause performance degradation if used too often,
-        we need something better
+        This algorithm is taken from zope.intid but it will cause performance
+        degradation due to fragmentation if used too often, we need something
+        better eventually
         """
         nextid = self._v_nextid
         while True:
@@ -40,6 +47,13 @@ class IdStore(persistent.Persistent):
             nextid = None
 
     def add(self, obj):
+        """
+        Add object to the storage
+
+        :param obj: Object to store (persistent.Persistent but not necessarily)
+        :return: Unique ID
+        :rtype: int
+        """
         while True:
             uid = self._generateId()
             if self.tree.insert(uid, obj):  # We use this feature of BTrees to avoid conflicts
@@ -47,6 +61,10 @@ class IdStore(persistent.Persistent):
                 return uid
 
     def remove(self, iobj):
+        """
+        Remove object from the storage
+        :param obj: Object or its integer unique id
+        """
         if type(iobj) in (int, long):
             del self.tree[iobj]
         elif hasattr(iobj, "_v_uid"):
@@ -56,6 +74,9 @@ class IdStore(persistent.Persistent):
             raise TypeError("Argument should be either uid or object itself")
 
     def __getitem__(self, uid):
+        """
+        :param int uid: Get object by its unique ID
+        """
         return self.tree[uid]
 
     def __len__(self):
