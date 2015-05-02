4ef2cd5c8433855df741ff34783312b3812a6727
Michael Egorov <michwill@gmail.com>
2015-05-02 14:37:55 -0700

    Permittable storage: first draft

--- zerodb/permissions/digest.py
+++ zerodb/permissions/digest.py
@@ -4,6 +4,9 @@ import persistent
 import transaction
 import ZODB
 from os import path
+from ZEO.auth.auth_digest import StorageClass as AuthStorageClass
+from ZEO.auth import register_module
+from ZEO.auth.auth_digest import DigestClient
 from ZODB import FileStorage
 
 from zerodb.crypto import rand
@@ -240,3 +243,49 @@ class PermissionsDatabase(object):
         if username not in usernames:
             raise LookupError("No such user: %s" % username)
         return users[uid]
+
+
+class PermittableZEOStorage(AuthStorageClass):
+
+    def _check_permission(self, name, oid):
+        print name, oid.encode("hex"), getattr(self, "username", None)
+        return True
+
+    def auth_response(self, resp):
+        """
+        Record username to the connection.
+        We need to know it to figure out who reads what
+        """
+
+        result = AuthStorageClass.auth_response(self, resp)
+
+        username = resp[0] if self.authenticated else None
+        self.username = username
+
+        return result
+
+    def register(self, storage_id, read_only):
+        AuthStorageClass.register(self, storage_id, read_only)
+        print self.username, self.storage
+        # Need to open db.
+        # If user doesn't exist, create with a new id (locking the db)
+        # Get the id, close connection, id goes to this class
+
+    # methods
+    # loadEx, loadBefore, deleteObject, storea, restorea, storeBlobEnd, storeBlobShared, sendBlob, loadSerial?
+    # get_info shoud show auth support
+    # need get_private_root and get_public_root
+    # store with permissions, e.g. obj -> struct.pack("Q", user_id) + data -> xdata[:8] + xdata[8:]
+
+    def loadEx(self, oid):
+        if self._check_permission("loadEx", oid):
+            return AuthStorageClass.loadEx(self, oid)
+
+    def storea(self, oid, serial, data, id):
+        if self._check_permission("storea", oid):
+            return AuthStorageClass.storea(self, oid, serial, data, id)
+
+
+def register_auth():
+    register_module("permidigest", PermittableZEOStorage, DigestClient, PermissionsDatabase)
+    # register_module("permidigest", PermittableZEOStorage, DigestClient, DigestDatabase)
