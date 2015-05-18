c6ffb3eea72c411fd90893962ae4340f30e5f6b1
Michael Egorov <michwill@gmail.com>
2015-05-17 19:37:25 -0700

    Server moved to a separate repo - remove

--- server/mkpub.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python2
-
-from zerodb.crypto import ecc
-
-passphrase = raw_input("Enter your passphrase: ")
-key = ecc.private(passphrase)
-print "Your hex public key:", key.get_pubkey().encode("hex")
