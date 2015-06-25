53a57f67386f64c12a21d6556fc4111f67d20dcf
Michael Egorov <michwill@gmail.com>
2015-06-24 20:02:19 -0700

    API minor changes

--- zerodb/appserver.py
+++ zerodb/appserver.py
@@ -3,6 +3,11 @@
 from flask import Flask, jsonify, session, request
 import zerodb
 
+try:
+    import simplejson as json
+except:
+    import json
+
 PORT = 2015
 HOST = "127.0.0.1"
 DEBUG = True
@@ -15,19 +20,16 @@ dbs = {}
 @app.route("/_connect", methods=["GET", "POST"])
 def connnect():
     if request.method == "GET":
-        username = request.args.get("username")
-        passphrase = request.args.get("passphrase")
-        host = request.args.get("host")
-        port = request.args.get("port")
-
+        req = request.args
     elif request.method == "POST":
-        username = request.form.get("username")
-        passphrase = request.form.get("passphrase")
-        host = request.form.get("host")
-        port = request.form.get("port")
-
+        req = request.form
     else:
         return jsonify(ok=0)
+
+    username = req.get("username")
+    passphrase = req.get("passphrase")
+    host = req.get("host")
+    port = req.get("port")
 
     if not (username and passphrase and host and port):
         return jsonify(ok=0, message="Incomplete login information")
@@ -36,7 +38,7 @@ def connnect():
         print host, port, username, passphrase
         db = zerodb.DB((host, port), username=username, password=passphrase)
     except Exception, e:
-        return jsonify(ok=0, message=str(e))
+        return jsonify(ok=0, message=str(e), error_type=e.__class__.__name__)
 
     session["username"] = username
     dbs[username] = db
@@ -46,6 +48,22 @@ def connnect():
 
 @app.route("/<table_name>/_find")
 def find(table_name):
+    if request.method == "GET":
+        req = request.args
+    elif request.method == "POST":
+        req = request.form
+    else:
+        return jsonify(ok=0)
+
+    criteria = json.loads(req.get("criteria"))
+
+    skip = req.get("skip")
+    if skip:
+        skip = int(skip)
+    limit = req.get("limit")
+    if limit:
+        limit = int(limit)
+
     return jsonify(hello="world")
 
 
