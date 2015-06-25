bfa9471b01fb01a2f8f45382eaa6a23e74e65820
Michael Egorov <michwill@gmail.com>
2015-06-24 18:41:59 -0700

    Mongo-style json query parsing

---
+++ test/test_query_json.py
@@ -0,0 +1,25 @@
+from StringIO import StringIO
+from zerodb.catalog import query_json as qj
+from zerodb.catalog.query import optimize
+from zerodb.catalog.query import Gt, Contains
+from zerodb.catalog.query import Not, Eq, InRange
+
+
+def qtree(q):
+    q = optimize(q)
+    f = StringIO()
+    q.print_tree(out=f)
+    f.seek(0)
+    out = f.read()
+    f.close()
+    return out
+
+
+def test_query_json():
+    q1 = qj.compile({"$and": [{"field1": {"$gt": 10}}, {"field2": {"$eq": "hello"}}]})
+    q2 = Gt("field1", 10) & Eq("field2", "hello")
+    assert qtree(q1) == qtree(q2)
+
+    q1 = qj.compile({"$not": {"$or": [{"field1": {"$range": [1, 15]}}, {"field2": {"$text": "hello"}}]}})
+    q2 = Not(InRange("field1", 1, 15) | Contains("field2", "hello"))
+    assert qtree(q1) == qtree(q2)
---
+++ zerodb/catalog/query_json.py
@@ -0,0 +1,55 @@
+from query import Eq, Lt, Gt, And, Or, Not, InRange, Contains,\
+                  NotEq, Le, Ge, NotInRange, DoesNotContain,\
+                  Any, All, NotAny, NotAll
+
+"""
+Query examples (similar to http://docs.mongodb.org/manual/reference/operator/query/):
+
+    {"$and": [{"field1": {"$gt": 10}}, {"field2": {"$text": "hello"}}]}
+    {field: {"$range": [1, 10]}}
+"""
+
+logical_operators = {
+        "$and": And,
+        "$or": Or,
+        "$not": Not}
+
+field_operators = {
+        "$eq": Eq,
+        "$ne": NotEq,
+        "$lt": Lt,
+        "$lte": Le,
+        "$gt": Gt,
+        "$gte": Ge,
+        "$range": InRange,
+        "$nrange": NotInRange,
+        "$text": Contains,
+        "$ntext": DoesNotContain,
+        "$in": Any,
+        "$all": All,
+        "$nany": NotAny,
+        "$nin": NotAll}
+
+
+def compile(q):
+    """
+    :param dict q: deserialized json
+    :returns: query object
+    :rtype: zerodb.catalog.query.Query
+    """
+    assert len(q) == 1
+    key = q.keys()[0]
+
+    if key in logical_operators:
+        if isinstance(q[key], list):
+            return logical_operators[key](*map(compile, q[key]))
+        else:
+            return logical_operators[key](compile(q[key]))
+    else:
+        assert isinstance(q[key], dict)
+        assert len(q[key]) == 1
+        opkey = q[key].keys()[0]
+        params = q[key][opkey]
+        if not isinstance(params, list):
+            params = [params]
+        return field_operators[opkey](key, *params)
