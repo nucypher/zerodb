025168d37c0b5b018a9135f96efd66ba80cf2b5a
Michael Egorov <michwill@gmail.com>
2015-08-02 12:21:30 -0700

    Tests pass

--- test/test_proxy_reencryption.py
+++ test/test_proxy_reencryption.py
@@ -1,37 +1,51 @@
 from hashlib import sha256
-from zerodb.crypto.afgh_pre import afgh_pre
+
 
 test_message = sha256("Hello world").digest()
+afgh_pre = None
+alice = None
+bob = None
 
-alice = afgh_pre.Key.make_priv()
-bob = afgh_pre.Key.make_priv()
+
+def init():
+    global afgh_pre, alice, bob
+
+    if afgh_pre is None:
+        from zerodb.crypto.afgh_pre import afgh_pre
+        alice = afgh_pre.Key.make_priv()
+        bob = afgh_pre.Key.make_priv()
 
 
 def test_encrypt_decrypt():
+    init()
     emsg = alice.encrypt(test_message)
     assert isinstance(emsg, basestring)
     assert alice.decrypt_my(emsg) == test_message
 
 
 def test_reencrypt():
+    init()
     emsg = alice.encrypt(test_message)
     remsg = alice.re_key(bob).reencrypt(emsg)
     assert bob.decrypt_re(remsg) == test_message
 
 
 def test_load_priv():
+    init()
     dump = alice.dump_priv()
     assert isinstance(dump, basestring)
     assert afgh_pre.Key.load_priv(dump).priv == alice.priv
 
 
 def test_load_pub():
+    init()
     dump = alice.dump_pub()
     assert isinstance(dump, basestring)
     assert afgh_pre.Key.load_pub(dump).pub == alice.pub
 
 
 def test_reencryption_full():
+    init()
     # Alice encrypts message and makes reencryption key fpr Bob
     emsg = alice.encrypt(test_message)
     re_key = alice.re_key(bob.dump_pub())
