16a89e78e70f8d1ac1b0832867244229f1127c5d
Michael Egorov <michwill@gmail.com>
2016-02-27 18:46:58 +0000

    Test low-level index methods

--- test/test_wiki_text.py
+++ test/test_wiki_text.py
@@ -34,3 +34,31 @@ def test_unindex(wiki_db):
     with transaction.manager:
         key, _ = get_one(wiki_db)
         get_cat(wiki_db).unindex_doc(key)
+
+
+def test_idf2(wiki_db):
+    with transaction.manager:
+        text = "Africa Asia NonExistingWord"
+        index = get_cat(wiki_db).index
+        wids = index._lexicon.sourceToWordIds(text)
+        assert len(wids) == 3
+        wids = index._remove_oov_wids(wids)
+        assert len(wids) == 2
+        idfs = map(index.idf2, wids)
+        assert all(idf > 0 for idf in idfs)
+
+
+def test_query_weight(wiki_db):
+    index = get_cat(wiki_db).index
+    assert index.query_weight("Africa Asia") > 0
+
+
+def test_search_wids(wiki_db):
+    index = get_cat(wiki_db).index
+    text = "Africa Asia"
+    wids = index._lexicon.sourceToWordIds(text)
+    for wordinfo, idf in index._search_wids(wids):
+        assert idf > 0
+        weight, docid = iter(wordinfo).next()
+        assert weight < 0
+        assert docid >= 0
