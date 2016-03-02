94b460fe7e05fb7dc9a42965c54284aaa4f77f1a
Michael Egorov <michwill@gmail.com>
2016-03-02 13:56:58 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name="zerodb",
-    version="0.96.20",
+    version="0.96.21",
     description="End-to-end encrypted database",
     author="ZeroDB Inc.",
     author_email="michael@zerodb.io",
