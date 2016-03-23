f457b06dd1dd9d3b1d3577b5a8415868ff0cb3d8
Stefan H. Holek <sholek@eastwestsounds.com>
2016-03-23 12:06:38 +0100

    Fall back to pycryptodome if aes256gcm-nacl cannot be built.

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+from os.path import isdir, isfile, join
+from distutils import log
+
 from setuptools import setup, find_packages
 
 INSTALL_REQUIRES = [
@@ -18,10 +21,39 @@ INSTALL_REQUIRES = [
     'ecdsa',
     'zope.event',
     'zope.lifecycleevent',
-    'aes256gcm-nacl',
     'ZEO',
     'six'
 ]
+
+
+# This is to avoid build errors on brand new Amazon Ubuntu instances
+# which may not have libffi-dev installed.
+#
+# If we have neither cffi nor ffi.h we fall back to pycryptodome.
+# Note that the warning is only visible if pip is run with -v.
+
+def have_cffi():
+    try:
+        import cffi
+    except ImportError:
+        return False
+    else:
+        return True
+
+def have_ffi_h():
+    include_dirs = ["/usr/include", "/usr/local/include"]
+    for dir in include_dirs:
+        if isdir(dir):
+            if isfile(join(dir, "ffi.h")) or isfile(join(dir, "ffi", "ffi.h")):
+                return True
+    return False
+
+if have_cffi() or have_ffi_h():
+    INSTALL_REQUIRES.append("aes256gcm-nacl")
+else:
+    log.warn("warning: *** ffi.h not found - aes256gcm-nacl optimization disabled ***")
+    INSTALL_REQUIRES.append("pycryptodome")
+
 
 setup(
     name="zerodb",
