0e84e6c55c34e10a1a6f2e6ef9a47a358a2062a5
Stefan H. Holek <sholek@eastwestsounds.com>
2016-03-23 21:22:22 +0100

    Enable aes256gcm-nacl only if cffi exists or can likely be built.
    Uses the pkg-config utility to determine the include path for ffi.h
    on systems that support it.

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+import sys
+import os
+import subprocess
+import errno
+import tempfile
+
+from distutils import ccompiler, log
 from setuptools import setup, find_packages
 
 INSTALL_REQUIRES = [
@@ -18,10 +25,83 @@ INSTALL_REQUIRES = [
     'ecdsa',
     'zope.event',
     'zope.lifecycleevent',
-    'aes256gcm-nacl',
     'ZEO',
     'six'
 ]
+
+
+# The following is to avoid build errors on brand new Amazon Ubuntu
+# instances which may not have libffi-dev installed.
+
+# Function copied from cffi 1.5.2
+def _ask_pkg_config(resultlist, option, result_prefix='', sysroot=False):
+    pkg_config = os.environ.get('PKG_CONFIG', 'pkg-config')
+    try:
+        p = subprocess.Popen([pkg_config, option, 'libffi'],
+                             stdout=subprocess.PIPE)
+    except OSError as e:
+        if e.errno not in [errno.ENOENT, errno.EACCES]:
+            raise
+    else:
+        t = p.stdout.read().decode().strip()
+        p.stdout.close()
+        if p.wait() == 0:
+            res = t.split()
+            # '-I/usr/...' -> '/usr/...'
+            for x in res:
+                assert x.startswith(result_prefix)
+            res = [x[len(result_prefix):] for x in res]
+            #print 'PKG_CONFIG:', option, res
+            #
+            sysroot = sysroot and os.environ.get('PKG_CONFIG_SYSROOT_DIR', '')
+            if sysroot:
+                # old versions of pkg-config don't support this env var,
+                # so here we emulate its effect if needed
+                res = [path if path.startswith(sysroot)
+                            else sysroot + path
+                         for path in res]
+            #
+            resultlist[:] = res
+
+
+def can_build_cffi():
+    # Windows hopefully grabs binary wheels
+    if sys.platform == "win32":
+        return True
+
+    # Include dirs copied from cffi 1.5.2
+    include_dirs = ["/usr/include/ffi", "/usr/include/libffi", "/usr/local/include"]
+    _ask_pkg_config(include_dirs, "--cflags-only-I", "-I", sysroot=True)
+
+    cc = ccompiler.new_compiler()
+    cc.include_dirs = [str(x) for x in include_dirs] # PY2
+
+    with tempfile.NamedTemporaryFile(suffix=".c") as f:
+        f.write('#include "ffi.h"\nvoid foo(void){}\n')
+        f.flush()
+        try:
+            cc.compile([f.name])
+            return True;
+        except ccompiler.CompileError:
+            return False
+
+
+def have_cffi():
+    try:
+        import cffi
+        return True
+    except ImportError:
+        return False
+
+
+# If we have neither cffi nor ffi.h we fall back to pycryptodome.
+# Note that the warning is only visible if pip is run with -v.
+
+if have_cffi() or can_build_cffi():
+    INSTALL_REQUIRES.append("aes256gcm-nacl")
+else:
+    log.warn("warning: ffi.h not found: aes256gcm-nacl optimization disabled")
+
 
 setup(
     name="zerodb",
