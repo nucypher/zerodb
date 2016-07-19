27181c30f3fff26def2def19f5e9d7612c7af751
Jim Fulton <jim@zope.com>
2016-07-19 19:47:36 -0400

    Added a server convenience function
    Adapted from ZEO.server.

--- zerodb/__init__.py
+++ zerodb/__init__.py
@@ -1,1 +1,11 @@
 from .db import DB
+
+def server(path=None, blob_dir=None, storage_conf=None, zeo_conf=None,
+           port=0, threaded=True, **kw):
+    import os, zerodb.forker
+    if storage_conf is None and path is None:
+        storage_conf = '<mappingstorage>\n</mappingstorage>'
+
+    return zerodb.forker.start_server(
+        storage_conf, zeo_conf, port, keep=True, path=path,
+        blob_dir=blob_dir, suicide=False, threaded=threaded, **kw)
