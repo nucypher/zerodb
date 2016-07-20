bd9be5c20d9808360ff8498faf8115ec64ace709
Jim Fulton <jim@zope.com>
2016-07-20 13:21:06 -0400

    Added some assertions and try to create a non-root user.
    Next step is to connect using Zerodb and see if it works. :)

--- tests/test_ssl.py
+++ tests/test_ssl.py
@@ -1,9 +1,18 @@
 """Test new SSL-based access
 """
+import os
 
-import zerodb
+from ZODB.utils import z64
 import ZEO
 import ZEO.tests.testssl
+
+import zerodb
+import zerodb.permissions.base
+
+here = os.path.dirname(__file__)
+def pem_data(name):
+    with open(os.path.join(here, name + '.pem')) as f:
+        return f.read()
 
 def test_basic():
     # zerodb.server took care of setting up a databasw with a root
@@ -13,15 +22,18 @@ def test_basic():
 
     # Create an admin client.  Admin data aren't encrypted, so we use
     # a regular ZEO client.
-    admin = ZEO.client(addr, ssl = ZEO.tests.testssl.client_ssl())
+    admin_db = ZEO.DB(addr, ssl = ZEO.tests.testssl.client_ssl())
+    with admin_db.transaction() as conn:
+        root = conn.root.users[z64]
+        assert len(conn.root.users) == 1
+        [root_der] = root.certs
+        assert len(root.certs) == 1
+        assert conn.root.certs.data.strip() == root.certs[root_der].strip()
+        assert conn.root.users_by_der[root_der] is root
+        assert len(conn.root.users_by_der) == 1
 
+        # Let's add a user:
+        zerodb.permissions.base.add_user(conn, 'user1', pem_data('cert0'))
 
-
-
-
-
-
-
-
-    admin.close()
+    admin_db.close()
     stop()
