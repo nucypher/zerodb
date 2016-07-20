6014a655228b9333522ade5de9b85951b5038e94
Jim Fulton <jim@zope.com>
2016-07-20 13:15:40 -0400

    Abstracted some functionality and added add_user

--- zerodb/permissions/base.py
+++ zerodb/permissions/base.py
@@ -1,6 +1,27 @@
+"""User database management
+
+Database root objects::
+
+  users: {userid -> User}
+  users_by_der: {der -> User}
+  certs: Certs
+
+Where DER is a DER encoding of a cert.
+
+The root user's user id is z64 (aka 0).
+
+All other user's ids are the oids of their root folders.
+
+Users have certs: {der -> pem_data}
+
+The Certs object is just a persistent container for the concatenation
+of all of the user certs.
+
+"""
+
 import ssl
 
-from BTrees.OOBTree import BTree, TreeSet
+from BTrees.OOBTree import BTree
 from ZODB.utils import z64
 import persistent
 import persistent.mapping
@@ -11,32 +32,57 @@ from . import subdb
 
 class User(persistent.Persistent):
 
-    def __init__(self, id, username, root):
+    def __init__(self, username, root):
         """
         :param str od: User id
         :param str username: User name
         :param PersistentMapping: User's database root
         """
-        self.id = id
         self.username = username
         self.root = root
+        self.id = root._p_oid
         self.certs = {} # {cert_der -> cert_pem}
 
-def init_db(db, uname, pem_data):
+class Certs(persistent.Persistent):
 
-    user = User(z64, uname, persistent.mapping.PersistentMapping())
+    def __init__(self):
+        self.data = ''
+
+    def add(self, pem_data):
+        self.data += '\n\n' + pem_data
+
+def get_der(pem_data):
     context = ssl.create_default_context(cadata=pem_data)
-    [cert_der] = context.get_ca_certs(1)
-    user.certs[cert_der] = pem_data
+    [cert_der] = context.get_ca_certs(1) # TCBOO
+    return cert_der
 
+def add_user(conn, uname, pem_data, user_id=None):
+    root = persistent.mapping.PersistentMapping()
+    conn.add(root)
+
+    user = User(uname, root)
+    if user_id:
+        if user_id in conn.root.users:
+            raise ValueError("User id already used", user_id)
+        user.id = user_id # root user is special
+
+    conn.root.users[user.id] = user
+
+    cert_der = get_der(pem_data)
+    if cert_der in conn.root.users_by_der or cert_der in user.certs:
+        raise ValueError("SSL certificate id already used",
+                         pem_data, user.id)
+    conn.root.users_by_der[cert_der] = user
+    user.certs[cert_der] = pem_data
+    conn.root.certs.add(pem_data)
+
+def init_db(db, uname, pem_data):
     with db.transaction() as conn:
         conn.root.users        = BTree()   # {uid -> user}
         conn.root.users_by_der = BTree()   # {cert_der -> user}
-        conn.root.admins       = TreeSet() # {admin_user_id}
+        conn.root.certs =        Certs()   # {cert_der -> user}
 
-        conn.root.users[user.id] = conn.root.users_by_der[cert_der] = user
-        conn.root.admins.add(user.id)
-        conn.root.certs = pem_data
+        add_user(conn, uname, pem_data, z64)
 
 def init_db_script():
     import argparse
